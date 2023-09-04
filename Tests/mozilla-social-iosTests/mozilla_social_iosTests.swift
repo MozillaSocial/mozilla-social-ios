07826146c9bb2f4e29b88834a7713c8e3626b8c7
Gio <g.ruscigno@icloud.com>
2023-09-04 13:21:35 -0500

    Initial Commit

---
+++ Tests/mozilla-social-iosTests/mozilla_social_iosTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import mozilla_social_ios
+
+final class mozilla_social_iosTests: XCTestCase {
+    func testExample() throws {
+        // XCTest Documentation
+        // https://developer.apple.com/documentation/xctest
+
+        // Defining Test Cases and Test Methods
+        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    }
+}
