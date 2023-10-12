1b9e1e49338c633035b4f60ff3e2f12af71a883f
Gio <g.ruscigno@icloud.com>
2023-10-12 01:48:29 -0500

    feat(analytics): add a simple user type to mimic logged a logged in user

--- Demo/MozillaSocial-iOS/MoSoContent/AppConfigurator.swift
+++ Demo/MozillaSocial-iOS/MoSoContent/AppConfigurator.swift
@@ -9,13 +9,28 @@ import SwiftUI
 
 // TODO: this is a barebone example for now. Session will be received from login and auth services as needed
 struct AppConfigurator {
+    let session: MoSoSession
     let analyticsProvider: AnalyticsProvider
     let discoverProvider: DiscoverProvider
 
+    var loggedIn: Bool {
+        session.isLoggedIn
+    }
 
     init() {
-        analyticsProvider = AnalyticsProvider()
-        self.discoverProvider  = DiscoverProvider(session: MoSoSession(isLoggedIn: true))
+        session = MoSoSession()
+        analyticsProvider = AnalyticsProvider(session: session)
+        discoverProvider  = DiscoverProvider(session: session, tracker: analyticsProvider.makeDiscoverTracker())
+        // TODO: replace this with actual user coming from login
+        login(user: MoSoUser(username: "test-user@mozilla.com", identifier: "12345678"))
+    }
+
+    func login(user: MoSoUser) {
+        session.loggedIn(user)
+    }
+
+    func logout() {
+        session.loggedOut()
     }
 
     @MainActor
--- Sources/MoSoCore/MoSoUser.swift
+++ Sources/MoSoCore/MoSoUser.swift
@@ -1,8 +1,14 @@
-//
-//  File.swift
-//  
-//
-//  Created by Giorgio Ruscigno on 10/12/23.
-//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-import Foundation
+// TODO: this very simple implementation will change
+public class MoSoUser {
+    public let username: String
+    public let identifier: String
+
+    public init(username: String, identifier: String) {
+        self.username = username
+        self.identifier = identifier
+    }
+}
