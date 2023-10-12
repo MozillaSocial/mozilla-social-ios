5da09180fed7ac12a3951d72454ce0fea1044e84
Gio <g.ruscigno@icloud.com>
2023-10-12 12:19:13 -0500

    feat(analytics): added preview placeholders for RecommendationRow and DiscoverTracker. Also removed some leftover comments

---
+++ Sources/DiscoverKit/Previews/PreviewTracker.swift
@@ -0,0 +1,13 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+import MoSoAnalytics
+
+/// Placeholder `DiscoverTracker` used in previews
+struct PreviewTracker: DiscoverTracker {
+    func trackRecommendationOpen(recommendationID: String) {}
+    func trackRecommendationShare(recommendationID: String) {}
+    func trackRecommendationBookmark(recommendationID: String) {}
+    func trackRecommendationImpression(recommendationID: String) {}
+}
