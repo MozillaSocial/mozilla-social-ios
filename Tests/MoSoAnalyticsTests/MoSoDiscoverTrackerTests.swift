5301563b370bd8812bfd7e9eab1135d7f0667f3c
Gio <g.ruscigno@icloud.com>
2023-10-17 00:28:39 -0500

    feat(analytics): update MoSoDiscoverTrackerTests, add more unit tests

--- Tests/MoSoAnalyticsTests/MoSoDiscoverTrackerTests.swift
+++ Tests/MoSoAnalyticsTests/MoSoDiscoverTrackerTests.swift
@@ -12,7 +12,7 @@ class MoSoDiscoverTrackerTests: XCTestCase {
         let user = MoSoUser(username: "example@username.org", identifier: "myNickname")
         let baseTracker = MockBaseTracker(session: MoSoSession(user: user))
         let discoverTracker = MoSoDiscoverTracker(baseTracker: baseTracker)
-        baseTracker.engagementExpectation = expectation(description: "Engagement event")
+        baseTracker.engagementExpectation = expectation(description: "open recommendation")
         // When
         discoverTracker.trackRecommendationOpen(recommendationID: "Rec001")
         // Then
@@ -21,6 +21,87 @@ class MoSoDiscoverTrackerTests: XCTestCase {
         XCTAssertEqual(baseTracker.recommendationID, "Rec001")
         XCTAssertEqual(baseTracker.uiIdentifier, "discover.recommendation.open")
         XCTAssertEqual(baseTracker.engagementAction, .general)
+        XCTAssertNil(baseTracker.postID)
+        XCTAssertNil(baseTracker.additionalInfo)
+        XCTAssertNil(baseTracker.associatedValue)
+        waitForExpectations(timeout: 1)
+    }
+
+    func testTrackRecommendationShare() {
+        // Given
+        let user = MoSoUser(username: "example@username.org", identifier: "myNickname")
+        let baseTracker = MockBaseTracker(session: MoSoSession(user: user))
+        let discoverTracker = MoSoDiscoverTracker(baseTracker: baseTracker)
+        baseTracker.engagementExpectation = expectation(description: "share recommendation")
+        // When
+        discoverTracker.trackRecommendationShare(recommendationID: "Rec001")
+        // Then
+        XCTAssertEqual(baseTracker.mastodonAccountHandle, "example@username.org")
+        XCTAssertEqual(baseTracker.mastodonAccountID, "myNickname")
+        XCTAssertEqual(baseTracker.recommendationID, "Rec001")
+        XCTAssertEqual(baseTracker.uiIdentifier, "discover.recommendation.share")
+        XCTAssertEqual(baseTracker.engagementAction, .share)
+        XCTAssertNil(baseTracker.postID)
+        XCTAssertNil(baseTracker.additionalInfo)
+        XCTAssertNil(baseTracker.associatedValue)
+        waitForExpectations(timeout: 1)
+    }
+
+    func testTrackRecommendationBookmark() {
+        // Given
+        let user = MoSoUser(username: "example@username.org", identifier: "myNickname")
+        let baseTracker = MockBaseTracker(session: MoSoSession(user: user))
+        let discoverTracker = MoSoDiscoverTracker(baseTracker: baseTracker)
+        baseTracker.engagementExpectation = expectation(description: "bookmark recommendation")
+        // When
+        discoverTracker.trackRecommendationBookmark(recommendationID: "Rec001")
+        // Then
+        XCTAssertEqual(baseTracker.mastodonAccountHandle, "example@username.org")
+        XCTAssertEqual(baseTracker.mastodonAccountID, "myNickname")
+        XCTAssertEqual(baseTracker.recommendationID, "Rec001")
+        XCTAssertEqual(baseTracker.uiIdentifier, "discover.recommendation.bookmark")
+        XCTAssertEqual(baseTracker.engagementAction, .bookmark)
+        XCTAssertNil(baseTracker.postID)
+        XCTAssertNil(baseTracker.additionalInfo)
+        XCTAssertNil(baseTracker.associatedValue)
+        waitForExpectations(timeout: 1)
+    }
+
+    func testTrackDiscoverScreenImpression() {
+        // Given
+        let user = MoSoUser(username: "example@username.org", identifier: "myNickname")
+        let baseTracker = MockBaseTracker(session: MoSoSession(user: user))
+        let discoverTracker = MoSoDiscoverTracker(baseTracker: baseTracker)
+        baseTracker.impressionExpectation = expectation(description: "discover screen impression")
+        // When
+        discoverTracker.trackDiscoverScreenImpression()
+        // Then
+        XCTAssertEqual(baseTracker.mastodonAccountHandle, "example@username.org")
+        XCTAssertEqual(baseTracker.mastodonAccountID, "myNickname")
+        XCTAssertEqual(baseTracker.uiIdentifier, "discover.screen.impression")
+        XCTAssertNil(baseTracker.postID)
+        XCTAssertNil(baseTracker.additionalInfo)
+        XCTAssertNil(baseTracker.associatedValue)
+        XCTAssertNil(baseTracker.recommendationID)
+        waitForExpectations(timeout: 1)
+    }
+
+    func testTrackRecommendationImpression() {
+        // Given
+        let user = MoSoUser(username: "example@username.org", identifier: "myNickname")
+        let baseTracker = MockBaseTracker(session: MoSoSession(user: user))
+        let discoverTracker = MoSoDiscoverTracker(baseTracker: baseTracker)
+        baseTracker.impressionExpectation = expectation(description: "recommendation impression")
+        // When
+        discoverTracker.trackRecommendationImpression(recommendationID: "Rec001")
+        // Then
+        XCTAssertEqual(baseTracker.mastodonAccountHandle, "example@username.org")
+        XCTAssertEqual(baseTracker.mastodonAccountID, "myNickname")
+        XCTAssertEqual(baseTracker.recommendationID, "Rec001")
+        XCTAssertEqual(baseTracker.uiIdentifier, "discover.recommendation.impression")
+        XCTAssertNil(baseTracker.postID)
+        XCTAssertNil(baseTracker.additionalInfo)
+        XCTAssertNil(baseTracker.associatedValue)
         waitForExpectations(timeout: 1)
     }
 }
