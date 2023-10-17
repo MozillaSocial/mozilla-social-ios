7cf198e0f521936ff984c88b2ffaea535c8659dc
Conor Masterson <cmasterson@mozilla.com>
2023-10-17 16:02:47 -0400

    Add swiftlint exclusion

--- Demo/MozillaSocial-iOS/MoSoContentUITests/MoSoContentUITests.swift
+++ Demo/MozillaSocial-iOS/MoSoContentUITests/MoSoContentUITests.swift
@@ -5,6 +5,7 @@
 import XCTest
 
 final class MoSoContentUITests: XCTestCase {
+    // swiftlint:disable overridden_super_call
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
 
@@ -14,6 +15,7 @@ final class MoSoContentUITests: XCTestCase {
         // In UI tests it’s important to set the initial state - such as interface orientation
         // required for your tests before they run. The setUp method is a good place to do this.
     }
+    // swiftlint:enable overridden_super_call
 
     func testLaunchPerformance() throws {
         if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
