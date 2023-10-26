091014660d9d6c3e205bea51fa52b1d1b23d69eb
Gio <g.ruscigno@icloud.com>
2023-10-26 12:47:00 -0500

    feat(danger): add danger package and file

---
+++ Demo/MozillaSocial-iOS/Dangerfile.swift
@@ -0,0 +1,63 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+import Danger
+import DangerSwiftCoverage
+import Foundation
+
+let danger = Danger()
+
+lint()
+// coverage()
+xcodeEnvironmentVariables()
+changedFiles()
+
+func changedFiles() {
+    message("Edited \(danger.git.modifiedFiles.count) files")
+    message("Created \(danger.git.createdFiles.count) files")
+}
+
+func lint() {
+    let violations = SwiftLint.lint(.all(directory: "./"), inline: true, configFile: ".swiftlint.yml", strict: true, quiet: false)
+    if violations.isEmpty {
+        message("No SwiftLint violations! 🎉")
+    }
+}
+
+func coverage() {
+    guard let xcresult = ProcessInfo.processInfo.environment["BITRISE_XCRESULT_PATH"]?.escapeString() else {
+        fail("Could not get the BITRISE_XCRESULT_PATH to generage code coverage")
+        return
+    }
+
+    Coverage.xcodeBuildCoverage(
+        .xcresultBundle(xcresult),
+        minimumCoverage: 50
+    )
+}
+
+func xcodeEnvironmentVariables() {
+    message("Checking XCode Environment Variables")
+
+    let snowplowPostPath = #"""
+            key = "SNOWPLOW_POST_PATH"
+            value = "com.snowplowanalytics.snowplow/tp2"
+            isEnabled = "YES">
+"""#
+
+    let modifiedXcodeSchemes = danger.git.modifiedFiles.filter { $0.contains(".xcscheme") }
+    for scheme in modifiedXcodeSchemes where danger.utils.readFile(scheme).contains(snowplowPostPath) {
+        fail("SNOWPLOW_POST_PATH must be disabled")
+    }
+}
+
+extension String {
+    // Helper function to escape (iOS) in our file name for xcov.
+    func escapeString() -> String {
+        var newString = self.replacingOccurrences(of: "(", with: "\\(")
+        newString = newString.replacingOccurrences(of: ")", with: "\\)")
+        newString = newString.replacingOccurrences(of: " ", with: "\\ ")
+        return newString
+    }
+}
---
+++ Demo/MozillaSocial-iOS/Package.swift
@@ -0,0 +1,29 @@
+// swift-tools-version:5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+import PackageDescription
+
+let package = Package(
+    name: "Danger",
+    platforms: [.iOS("16"), .macOS("13")],
+    products: [
+        .library(name: "DangerDeps", type: .dynamic, targets: ["DangerDependencies"]), // dev
+    ],
+    dependencies: [
+        .package(url: "https://github.com/danger/swift.git", exact: "3.16.0"), // dev
+        // .package(url: "https://github.com/f-meloni/danger-swift-coverage", exact: "1.2.1") // dev
+    ],
+    targets: [
+        .target(
+            name: "DangerDependencies",
+            dependencies: [
+                .product(name: "Danger", package: "swift"),
+                // .product(name: "DangerSwiftCoverage", package: "danger-swift-coverage")
+            ]
+        ) // dev
+    ]
+)
