17a28ad840cdd387cbd5b251222738dd13c72157
Conor Masterson <cmasterson@mozilla.com>
2023-11-08 11:08:25 -0500

    Remove old generated files

--- Sources/ReadingListKit/GraphQL/PocketGraph/Schema/Enums/PendingItemStatus.graphql.swift
+++
@@ -1,9 +0,0 @@
-// @generated
-// This file was automatically generated and should not be edited.
-
-import ApolloAPI
-
-public enum PendingItemStatus: String, EnumType {
-  case resolved = "RESOLVED"
-  case unresolved = "UNRESOLVED"
-}
--- Sources/ReadingListKit/GraphQL/PocketGraph/Schema/Interfaces/RemoteEntity.graphql.swift
+++
@@ -1,9 +0,0 @@
-// @generated
-// This file was automatically generated and should not be edited.
-
-import ApolloAPI
-
-public extension Interfaces {
-  /// Interface that all state based entities must implement
-  static let RemoteEntity = Interface(name: "RemoteEntity")
-}
