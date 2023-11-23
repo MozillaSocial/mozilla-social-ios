d80bc06bcd5dfd6e6acf415657da875d09bdbfbc
Conor Masterson <cmasterson@mozilla.com>
2023-11-23 09:52:30 -0500

    Open webview on tap

--- Sources/ReadingListKit/ReadingListCell.swift
+++ Sources/ReadingListKit/ReadingListCell.swift
@@ -3,14 +3,12 @@
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 import SwiftUI
+import Combine
 import DesignKit
 
 public struct ReadingListCell: View {
     let model: ReadingListCellViewModel
-
-    init(model: ReadingListCellViewModel) {
-        self.model = model
-    }
+    @Binding var selectedRow: ReadingListCellViewModel?
 
     public var body: some View {
         VStack {
@@ -61,17 +59,12 @@ public struct ReadingListCell: View {
                 .frame(width: Constants.buttonSize.width, height: Constants.buttonSize.height)
                 .buttonStyle(.plain)
                 .accessibility(addTraits: .isButton)
-                .simultaneousGesture(
-                    TapGesture().onEnded {
-                        print("Share")
-                    }
-                )
             }
         }
         .padding()
         .contentShape(Rectangle())
         .onTapGesture {
-            print("Tappp")
+            selectedRow = model
         }
     }
 
@@ -82,8 +75,4 @@ public struct ReadingListCell: View {
         static let height: CGFloat = 60
         static let buttonSize: CGSize = CGSize(width: 48, height: 48)
     }
-}
-
-#Preview {
-    ReadingListCell(model: ReadingListCellViewModel(id: "uniqueID", title: "A Very very very very long Test Title", subtitle: "Test Subtitle", contentURL: "www.getpocket.com", thumbnailURL: "https://helios-i.mashable.com/imagery/articles/05fACELrEVc4kAfNQbhhcVh/hero-image.fill.size_1248x702.v1667556469.png"))
 }
--- Sources/ReadingListKit/ReadingListView.swift
+++ Sources/ReadingListKit/ReadingListView.swift
@@ -3,38 +3,51 @@
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 import SwiftUI
+import Combine
 
 public struct ReadingListView: View {
     public init(model: ReadingListModel) {
         self.model = model
     }
     @ObservedObject var model: ReadingListModel
+    @State private var selectedRow: ReadingListCellViewModel?
 
     @Environment(\.horizontalSizeClass)
     var sizeClass
+    @Environment(\.openURL)
+    var openURL
 
     public var body: some View {
         NavigationStack {
             ScrollView {
                 LazyVStack {
                     ForEach(model.readingListItems, id: \.id) { viewModel in
-                        ReadingListCell(model: viewModel)
+                        ReadingListCell(model: viewModel, selectedRow: $selectedRow)
                             .navigationBarTitleDisplayMode(.inline)
                             .toolbarBackground(.visible, for: .navigationBar)
                             .toolbarBackground(Color(.mosoLayerColor1), for: .navigationBar)
                             .onAppear {
                                 model.didDisplay(item: viewModel)
                             }
+                            .if(sizeClass == .regular) { view in
+                                view.frame(width: 700, alignment: .center)
+                            }
                     }
                     if model.allItemsAreDownloaded() == false {
                         ProgressView()
                     }
                 }
-                .if(sizeClass == .regular) { view in
-                    view.frame(width: 700, alignment: .center)
-                }
+                .navigationTitle(Text("Reading List"))
             }
-            .navigationTitle(Text("Reading List"))
+            .onChange(of: selectedRow) { newValue in
+                guard let urlString = newValue?.contentURL,
+                      let url = URL(string: urlString) else {
+                    print("Could not create URL")
+                    return
+                }
+                selectedRow = nil
+                openURL(url)
+            }
         }
     }
 }
